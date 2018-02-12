b28bbbcd409b6d0882b6cf483a7c477d54b56ede
Dick Groenenberg <dickgroenenberg@gmail.com>
2018-02-12 15:37:06 +0100

    Update AutoBlast.sh

--- AutoBlast.sh
+++ AutoBlast.sh
@@ -2,29 +2,53 @@
 
 # dependencies: blastn: 2.6.0+
 #
-# loops through subfolder named "Query" and blasts all fasta files in it
-# fasta files should have the extension 'fasta'
-# results are written to 'Blast_results'
-# output format is XML (outfmt 5)
-# only the first 10 hits are kept
-#
-# Based on 'Blast_results' a subfolder 'Blast_sum' will be created
-# containing an excerpt of fasta_headers and full_description lines
+# Loops through all fasta files in the folder from where this script is initiated
+# and performs a remote Blast-search on the NCBI server for each of these files.
+# Input files should have the extensino 'fasta'
+# After this script has finished the following folders will be created:
+#	"Query" 			(the original fasta files)
+#	"Cleaned"			(fasta files without empty reads or indels: Blast input)
+#	"Blast_results"		(output from Blast-search in XML format: outfmt 5)
+#	"Blast_sum"			(summary containing first ten hits for each fasta header)
 #
 # usage: ./AutoBlast.sh
 #
 # author: D.S.J. Groenenberg
 # company: Naturalis Biodiversity Center
 
-mkdir Blast_results Blast_sum
+T="$(date +%s)"
+mkdir Query Cleaned Blast_results Blast_sum
+
+#---"Data cleaning"----------------------------------------------------------------------------
+# Remove indels "-" from fasta files
+# Remove failed reads from fasta
+# The fancy sed command searches for lines containing only n (ie. ^n$) and removes
+# that line and the line before.
+
+mv *.fasta Query/
+
 cd Query
 
 for i in *.fasta
-	do blastn -task blastn -db nt -query $i -max_target_seqs 10 -out ../Blast_results/"$i".blast -remote -outfmt 5
+do
+	tr -d "-" < $i | sed -n '/^n$/{s/.*//;x;d;};x;p;${x;p;}' | sed '/^$/d' > ../Cleaned/"$i"
+done
+
+#---"Blast cleaned sequences"----------------------------------------------------------------------------
+
+cd ../Cleaned
+
+for i in *.fasta
+do
+	blastn -task blastn -db nt -query $i -max_target_seqs 10 -out ../Blast_results/"$i".blast -remote -outfmt 5
 done
 
 cd ../Blast_results
 
 for i in *.blast
-	do egrep "<Iteration_query-def>|<Hit_def>" $i | tr ">" "<" | awk -F"<" '{print$3}' | sed 's/^I20/\nI20/g' > ../Blast_sum/"$i".sum.txt
+do 
+	egrep "<Iteration_query-def>|<Hit_def>" $i | tr ">" "<" | awk -F"<" '{print$3}' | sed 's/^I20/\nI20/g' > ../Blast_sum/"$i".sum.txt
 done
+
+T="$(($(date +%s)-T))"
+printf "Blast-search completed in: %02d hrs %02d min %02d sec\n" "$((T/3600%24))" "$((T/60%60))" "$((T%60))"
